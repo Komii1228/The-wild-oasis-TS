80c2e2ad8609609c400e0609c2066355fd01e94d
Miekole Parnes <miekole.parnes@steijnium.com>
2025-07-13 04:21:27 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+`;
+
+const Button = styled.button`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: purple;
+    color: white;
+`;
+
+const StyledApp = styled.div`
+    background-color: red;
+    padding: 20px;
+`;
+
+function App() {
+    return (
+        <StyledApp>
+            <H1>The Wild Oasis</H1>
+            <Button onClick={() => alert('Check in')}>Check in</Button>
+        </StyledApp>
+    );
+}
+
+export default App;
