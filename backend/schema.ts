8bf1d9091f6755bb9478b2cd55e43d400d93fe9f
Miekole Parnes <miekole.parnes@steijnium.com>
2025-07-18 02:10:24 +0800

    Removed custom backend  and frontend folder

--- backend/schema.ts
+++
@@ -1,8 +0,0 @@
-// backend/typeDefs.ts or directly in server.ts
-import { readFileSync } from "fs";
-import path from "path";
-import { gql } from "graphql-tag";
-
-export const typeDefs = gql(
-  readFileSync(path.join(__dirname, "graphql/schema.gql"), "utf-8")
-);
